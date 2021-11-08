e384fd28a2a22f820000352580ca24fde7c8ace0
Christoffer <links685@gmail.com>
2021-11-09 00:14:52 +0100

    shouldAddAndReturnQuestion() 🔴

--- src/test/java/no/kristiania/dao/QuestionDaoTest.java
+++ src/test/java/no/kristiania/dao/QuestionDaoTest.java
@@ -1,15 +1,27 @@
 package no.kristiania.dao;
 
+import no.kristiania.TestData;
 import no.kristiania.http.HttpServer;
+import no.kristiania.model.Question;
 import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 
 public class QuestionDaoTest {
     HttpServer server = new HttpServer();
 
     @Test
-    void shouldReturnFileWithController() {
+    void shouldAddAndReturnQuestion() throws SQLException {
+        Question question = new Question("Kan du backflip?");
+        QuestionDao dao = new QuestionDao(TestData.testDataSource());
+        dao.save(question, dao.getSaveStatementString());
 
-
+        assertThat(dao.retrieveById(question.getId(), dao.getRetrieveStatement()))
+                .hasNoNullFieldsOrProperties()
+                .usingRecursiveComparison()
+                .isEqualTo(question);
     }
 }
