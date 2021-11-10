d8124e993e24c8d5939db2a6cd8eceac0da229c8
Generoman <roman181@gmail.com>
2021-11-10 17:28:00 +0100

    minor notes, added TODO

--- src/main/java/no/kristiania/http/controllers/SingleQuestionController.java
+++ src/main/java/no/kristiania/http/controllers/SingleQuestionController.java
@@ -12,4 +12,11 @@ public class SingleQuestionController implements Controller{
     public HttpResponse handle(HttpRequest request) throws SQLException, IOException {
         return null;
     }
+
+    // TODO: implement methods
+
+// notes for later:
+//<input type="hidden" name="id" value="id">
+//<p><label>Title: <input type="text" name="questionTitle"></label></p>
+//<p><label>Question: <input type="text" name="questionText"></label></p>
 }
